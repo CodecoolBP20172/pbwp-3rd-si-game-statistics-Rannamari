85fe3be7767769fa1e0c6e388e03e59ed388ea0b
Annamari <riszt.annamari@hotmail.com>
2017-06-27 11:02:42 +0200

    commented versions

--- export.py
+++ export.py
@@ -1,2 +1,17 @@
+from reports import *
+# exporting files from reports.py to a newly created txt file
 
-# Export functions
+
+def export_data(file_name):
+    with open(file_name, 'w', newline="\n") as file:
+            file.writelines(str(count_games("game_stat.txt"))+"\n")
+            file.writelines(str(decide("game_stat.txt", 2004))+"\n")
+            file.writelines(str(get_latest("game_stat.txt"))+"\n")
+            file.writelines(str(count_by_genre("game_stat.txt", "Real-time strategy"))+"\n")
+            file.writelines(str(get_line_number_by_title("game_stat.txt", "Minecraft"))+"\n")
+            file.writelines(str(sort_abc("game_stat.txt"))+"\n")
+            file.writelines(str(get_genres("game_stat.txt"))+"\n")
+            file.writelines(str(when_was_top_sold_fps("game_stat.txt")))
+
+
+export_data()
--- printing.py
+++ printing.py
@@ -1,11 +1,20 @@
-from reports import *
+from reports import
+# importing functions from report.py
 
-print (get_line_number_by_title("game_stat.txt", "Minecraft"))
+print(reading_text("game_stat.txt"))
+
+print(count_games("game_stat.txt"))
+
+print(decide("game_stat.txt", 1998))
 
 print(get_latest("game_stat.txt"))
 
-print (count_by_genre("game_stat.txt", "Real-time strategy"))
+print(count_by_genre("game_stat.txt", "Real-time strategy"))
 
-print (get_genres("game_stat.txt"))
+print(get_line_number_by_title("game_stat.txt", "The Sims 2"))
 
-print (when_was_top_sold_fps("game_stat.txt"))
+print(sort_abc("game_stat.txt"))
+
+print(get_genres("game_stat.txt"))
+
+print(when_was_top_sold_fps("game_stat.txt"))
